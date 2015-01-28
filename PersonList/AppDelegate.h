5195d57acf3a945bf97d53674a54bda5ebaa6cf0
Joshua Howland <development@jkhowland.com>
2015-01-27 16:15:04 -0800

    Initial Commit (Single View Controller)

---
+++ PersonList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PersonList
+//
+//  Created by Joshua Howland on 1/27/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PersonList/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PersonList
+//
+//  Created by Joshua Howland on 1/27/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

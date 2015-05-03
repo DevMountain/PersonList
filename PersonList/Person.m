ed7a13cf538cb37d524ab94c38eccff321292d55
Caleb Hicks <calebhicks@gmail.com>
2015-05-02 18:04:30 -0600

    adds simple Person class

---
+++ PersonList/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  PersonList
+//
+//  Created by Joshua Howland on 1/27/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
